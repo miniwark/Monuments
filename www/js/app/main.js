b8f43e6b14c9bb84821df1139945141cf908873d
Bertrand Lecervoisier <miniwark@gmail.com>
2013-08-30 17:21:38 +0200

    Typo & TODOs

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -5,8 +5,19 @@
 
 require([
 	'app/templates',
-	'app/geolocation'
+	'app/geolocation',
+	'app/addresslookup',
+	'app/monumentslookup',
 ],
 function() {
 	//
 });
+
+
+// TODO:
+// - convert modules to Revealing Module Pattern
+// - move timers and events trigers here
+// in particular move the 5 minutes geolocation cycle here
+// and add a page refresh triger here if location change
+// - maybe move position from sessionstorage to localstorage
+// so the app have a fallback position will geting the new one
