a0acb6b1d0e52441829625b8d5564a6cd56cd627
Bertrand Lecervoisier <miniwark@gmail.com>
2013-09-02 11:56:45 +0200

    Use getJSON from Zepto to get datas

--- www/js/app/addresslookup.js
+++ www/js/app/addresslookup.js
@@ -1,33 +1,29 @@
 
 // get the country code & city by reverse geocoding
-// use OpenStreetMap datas hosted by MapQuest
+// use OpenStreetMap Nominatim datas hosted by MapQuest
 // http://wiki.openstreetmap.org/wiki/Nominatim#Reverse_Geocoding_.2F_Address_lookup
 // http://developer.mapquest.com/web/products/open/nominatim
 
 'use strict';
 
+
 define([], function() {
 
-    // generate the lookup address
-    var baseURL = 'http://open.mapquestapi.com/nominatim/v1/reverse.php?';
-    var format = 'json';
-    var latitude = window.sessionStorage.getItem('position_latitude');
-    var longitude = window.sessionStorage.getItem('position_longitude');        
-    var requestURL = baseURL + '&format=' + format + '&lat=' + latitude + '&lon=' + longitude;
+	// generate the lookup address
+	var baseURL = 'http://open.mapquestapi.com/nominatim/v1/reverse.php?';
+	var params = $.param({
+		format: 'json',
+		lat: window.sessionStorage.getItem('position_latitude'),
+		lon: window.sessionStorage.getItem('position_longitude')
+	});
+	var requestURL = baseURL + params + '&json_callback=?'
 
-	// parse JSON data from the external source asynchronously
-	var req = new XMLHttpRequest();
-	req.open('GET', requestURL, true);
-	req.onreadystatechange = function() {
-		if (req.readyState == 4 && req.status == 200) {
-			var responseJSON = JSON.parse(req.responseText);
-			// save the city in the sessionStorage
-			window.sessionStorage.setItem('position_country', responseJSON.address.country_code);
-			window.sessionStorage.setItem('position_city', responseJSON.address.city);
-		}
-	};
-	req.send(null);
-	
-	//TODO use $.getJSON instead
+    // get the JSONP data from the external source
+	$.getJSON(requestURL, function(jsonData){
+		var data = JSON.stringify(jsonData);
+		// save the city and contry in the localStorage
+		window.localStorage.setItem('position_country', jsonData.address.country_code);
+		window.localStorage.setItem('position_city', jsonData.address.city);
+	})
 	
 });
